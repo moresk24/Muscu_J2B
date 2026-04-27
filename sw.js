a366b02f4e4e11788b30d975dbc57b8bc5c3d4f4
moresk24 <76202944+moresk24@users.noreply.github.com>
2026-04-27 20:42:15 +0200

    Maj favicon

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE = 'muscu-j2b-v2';
+const CACHE = 'muscu-j2b-v3';
 const ASSETS = ['./', './index.html', './manifest.json',
-  './favicon-96x96.png', './web-app-manifest-192x192.png', './web-app-manifest-512x512.png'];
+  './favicon-16x16.png', './favicon-32x32.png', './apple-touch-icon.png',
+  './android-chrome-192x192.png', './android-chrome-512x512.png', './favicon.ico'];
 
 self.addEventListener('install', e =>
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
