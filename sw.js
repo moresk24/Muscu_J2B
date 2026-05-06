c20aaf2839307568e239481dfb754a5b67b8f1c5
moresk24 <76202944+moresk24@users.noreply.github.com>
2026-05-06 18:48:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,8 +9,15 @@ const ASSETS = ['./', './index.html', './styles.css', './data.js', './app.js', '
   './favicon-16x16.png', './favicon-32x32.png', './apple-touch-icon.png',
   './android-chrome-192x192.png', './android-chrome-512x512.png', './favicon.ico'];
 
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e =>
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim())));
 
 self.addEventListener('fetch', e => {
   // Ne pas intercepter les requêtes cross-origin (API Google Apps Script, etc.)
